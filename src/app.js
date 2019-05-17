aca9a958739c7bb0158dc38f1ad7445b15e3c197
swetha-shankar <swetha.shankar@digiscloud.com>
2019-05-17 16:36:56 +0530

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -8,6 +8,28 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req,res) => res.render('index', {title: 'Index'}));
+app.get('/', (req,res) => res.render('index', {title: 'Account Summary', accounts}));
+
+app.get('/savings', (req,res) => {
+    res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req,res) => {
+    res.render('account', { account: accounts.checking });
+});
+app.get('/credit', (req,res) => {
+    res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req,res) => {
+    res.render('profile', {users });
+});
+
 app.listen(3000, () => console.log('PS project running in port 3000....'));
